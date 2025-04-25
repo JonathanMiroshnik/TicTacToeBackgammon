0d29660b88f0b51d18994ad11d408dc46f1104c1
Jonathan Miroshnik <JonathanMiroshnik@gmail.com>
2025-04-25 17:48:20 +0300

    working on cell text alignment

--- client/src/components/Cell/Cell.tsx
+++ client/src/components/Cell/Cell.tsx
@@ -12,7 +12,9 @@ interface CellProps {
 function Cell({ cellState, row, column, setCell } : CellProps) {
     return (
         <button className="cell" onClick={() => setCell(row, column)}> 
-            { cellState.symbol !== Symbols._ ? Symbols[cellState.symbol].toString() + " " + cellState.totalDice : null }                 
+            {/* TODO: figure out aligning vertically symbol and number/counter */}
+            { cellState.symbol !== Symbols._ ? <b style={{fontSize:"30px", display: "inline-block"}} > { Symbols[cellState.symbol].toString() } </b>: null }
+            { cellState.symbol !== Symbols._ ? " " + cellState.totalDice : null }
             {/* { cellState.symbol !== Symbols._ ? <CellCounter score={cellState.totalDice} total={6} /> : null } */}
         </button>
     );
