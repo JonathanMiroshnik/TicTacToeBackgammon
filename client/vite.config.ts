7e5daf028636bc0818b0e0cb762f8caf15260edd
Jonathan Miroshnik <JonathanMiroshnik@gmail.com>
2025-05-05 16:28:08 +0300

    removed https client thingy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+// import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()], //mkcert() - https compliance
   server: {
-    // host: '0.0.0.0',
-    host: true,
+    host: '0.0.0.0',
+    // host: true,
     // host: '2a00:a041:28a0:7b00::1001',
     // host: '::',
     // https: true,
