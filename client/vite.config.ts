71c791b774fa129cfc6b9ee6f315bdcff6b7baef
Jonathan Miroshnik <JonathanMiroshnik@gmail.com>
2025-05-05 15:35:03 +0300

    client https

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
+    host: true,
     // host: '2a00:a041:28a0:7b00::1001',
     // host: '::',
+    // https: true,
     port: 51730,
     allowedHosts: ["yonatan-h110m-s2v.local", 'www.sensorcensor.xyz']
   },
