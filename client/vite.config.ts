0ef01b087c923e9951d215467bfaec8c0ac9e3a3
Jonathan Miroshnik <JonathanMiroshnik@gmail.com>
2025-05-05 15:24:51 +0300

    update to hosts in client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
     // host: '2a00:a041:28a0:7b00::1001',
     // host: '::',
     port: 51730
